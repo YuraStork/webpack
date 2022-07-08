b2404c338784337756a25b71ed38dfbe6d094f5a
Yura <yuraborisov19@gmail.com>
2022-07-08 10:06:02 +0300

    view password

--- src/models/input/index.tsx
+++ src/models/input/index.tsx
@@ -1,5 +1,6 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 import { InputWrapper } from "./styles"
+import View from '../../../public/assets/view.png';
 
 type InputProps = {
   type: string,
@@ -13,8 +14,14 @@ type InputProps = {
 }
 
 export const Input: FC<InputProps> = ({ label, ...rest }) => {
+  const [type, setType] = useState(rest.type || 'text');
+  const handleChangeType = () => setType(type === 'password' ? "text" : rest.type)
+  const isShow = type === 'password';
   return <InputWrapper isError={!!rest.error} hasValue={!!rest.value}>
-    <input {...rest} />
+    <input {...rest} type={type} />
     <label>{label}</label>
+    {rest.type === 'password' && <button onClick={handleChangeType} type="button">
+      <img src={View} alt="" width={10} height={10} />
+    </button>}
   </InputWrapper>
 }
